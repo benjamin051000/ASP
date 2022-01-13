91c325a4640eb555ed09e47ea6e4c288a9d4e521
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2022-01-13 13:41:25 -0500

    part 1 done

--- prelim/mapper.c
+++ prelim/mapper.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * @brief Read entire text file.
+ * @brief Read a text file into a buffer.
  * WARNING: You must free() the returned char*!
  */
-char* read(const char* filename) {
+char *read(const char *filename)
+{
     FILE *fp;
-    if((fp = fopen(filename, "r")) == NULL) {
+    if ((fp = fopen(filename, "r")) == NULL)
+    {
         printf("Error opening file.\n");
         exit(1);
     }
@@ -19,13 +22,14 @@ char* read(const char* filename) {
     fseek(fp, 0L, SEEK_SET);
 
     // Allocate buffer
-    char* buf = malloc(size * sizeof *buf);
+    char *buf = malloc(size * sizeof *buf);
 
     // Read data into buf
     unsigned index = 0;
     int curr_char;
 
-    while((curr_char = fgetc(fp)) != EOF) {
+    while ((curr_char = fgetc(fp)) != EOF)
+    {
         // Get next char
         buf[index] = curr_char;
 
@@ -34,17 +38,90 @@ char* read(const char* filename) {
 
     fclose(fp);
 
-    // Debug print
-    // printf("buf: \"%s\"\n", buf);
-
     return buf;
 }
 
-int main(void) {
+typedef struct entry
+{
+    int id;
+    char *action;
+    char *topic;
+} entry_t;
 
-    char* text = read("input.txt");
+int main(void)
+{
 
-    printf("text: \"%s\"\n", text);
+    char *text = read("input.txt");
+
+    printf("text: \"%s\"\n\n", text);
+
+    // List of entries
+    entry_t entries[100];
+    unsigned entries_size = 0;
+
+    // Parse the text to obtain tokens.
+    const char delims[] = "(), \n";
+    // printf("Tokens: ");
+
+    char *token = strtok(text, delims); // TODO where are these tokens stored? Heap?
+    int token_i = 0;
+
+    while (token != NULL)
+    {
+        // printf("\"%s\", ", token);
+
+        int id;
+        char *action;
+        char *topic;
+
+        if (token_i == 0)
+        {
+            // This is the first of the three tuple entries, which is the ID.
+            id = atoi(token);
+            token_i++;
+        }
+        else if (token_i == 1)
+        {
+            // This is the second tuple entry, which is the category.
+            action = token;
+            token_i++;
+        }
+        else
+        {
+            topic = token;
+            token_i = 0;
+
+            // Construct the entry
+            entries[entries_size++] = (entry_t){id, action, topic};
+        }
+
+        // Get next token
+        token = strtok(NULL, delims);
+    } // End of while
+
+    free(text);
+
+    // Print all the entries
+    printf("\n\nEntries:\n");
+    for (unsigned i = 0; i < entries_size; i++)
+    {
+        entry_t *e = &entries[i];
+
+        // Get cooresponding score from user action
+        int score;
+        if (strcmp(e->action, "P") == 0) // Post
+            score = 50;
+        else if (strcmp(e->action, "L") == 0) // Like
+            score = 20;
+        else if (strcmp(e->action, "D") == 0) // Dislike
+            score = -10;
+        else if (strcmp(e->action, "C") == 0) // Comment
+            score = 30;
+        else if (strcmp(e->action, "S") == 0) // Share
+            score = 40;
+
+        printf("(%d, %s, %d)\n", e->id, e->topic, score);
+    }
 
     return 0;
 }
