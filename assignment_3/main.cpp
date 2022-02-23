85504fb253a85f69e60603245ad535b75f4ec9ea
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2022-02-22 19:30:27 -0500

    print tokens

--- assignment_3/main.cpp
+++ assignment_3/main.cpp
@@ -1,4 +1,8 @@
+#include <fstream>
 #include <iostream>
+#include <regex>
+#include <sstream>
+#include <vector>
 
 #define DEBUG_MODE
 
@@ -11,13 +15,51 @@
 #endif
 
 using std::string;
+/**
+ * @brief Split by delimiter, return tokens.
+ * @param str String to be split.
+ * @param regex_str Regular expression of delimiters to split str by.
+ * @return std::vector<string> Iterator of tokens.
+ */
+std::vector<string> split(const std::string str, const std::string regex_str) {
+    const std::regex re(regex_str);
+    return {std::sregex_token_iterator(str.begin(), str.end(), re, -1),
+            std::sregex_token_iterator()};
+}
 
 int main(int argc, char* argv[]) {
+    // Ensure 2 CLI args are passed in
+    if (argc != 3) {
+        std::cerr << "Usage: ./transfProg <inputFile> <numWorkers>\n";
+        exit(EXIT_FAILURE);
+    }
 
+    // Extract CLI args
     const string INPUT_FILENAME = argv[1];
     const int NUM_WORKERS = std::stoi(argv[2]);
-    
-    D(std::cout << "input filename: " << INPUT_FILENAME << "\nnum workers: " << NUM_WORKERS << "\n";)
+
+    D(std::cout << "input filename: " << INPUT_FILENAME
+                << "\nnum workers: " << NUM_WORKERS << "\n";)
+
+    // Open file
+    std::ifstream file(INPUT_FILENAME);
+
+    if (!file) {
+        std::cerr << "ERROR: Invalid filename.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Use a string stream to read the buffered file.
+    std::stringstream ss;
+    ss << file.rdbuf();
+    std::string s = ss.str();
+
+    // Split tokens
+    auto tokens = split(s, " ");
+    D(for (auto& token : tokens) {
+        std::cout << token << "\n";
+    })
+
 
     D(std::cout << "Done.\n";)
     return 0;
