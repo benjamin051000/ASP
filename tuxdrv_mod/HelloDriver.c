937d1821ee63a14b49d6e926d4dc47e342140f7f
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2022-04-07 15:13:19 -0400

    test ioctl

--- tuxdrv_mod/HelloDriver.c
+++ tuxdrv_mod/HelloDriver.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <linux/ioctl.h>
+#include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
 #include <stdio.h>
+
+
+#define CLEAR_BUF _IOW('Z', 1, int)
+
 
 int main(void) {
 	int fd = open("/dev/tux0", O_WRONLY);
@@ -15,16 +21,32 @@ int main(void) {
 	char* msg = "testing writing\nHello there! \n... general kenobi??";
 	printf("message length: %lu\n", strlen(msg));
 	write(fd, (void*)msg, strlen(msg));
+	close(fd);
 
 	// Now, try to read
 	// First, reset the offset
 	//lseek(fd, 0, SEEK_SET); // lseek not working
 	
-	close(fd);
 	fd = open("/dev/tux0", O_RDONLY);
 	char buf[128];
 	read(fd, &buf, sizeof buf);
 	printf("Read msg: \"%s\"\n", buf);	
 	close(fd);
+
+	//////////////////////////////////
+	// Test ioctl
+	//////////////////////////////////
+	fd = open("/dev/tux0", O_RDWR);
+	int rc = ioctl(fd, CLEAR_BUF, 0);
+	if(rc == -1) {
+		printf("ERROR: ioctl failed\n");
+		close(fd);
+		return -1;
+	}
+	// Now, read to ensure it actually cleared the buffer.
+	read(fd, &buf, sizeof buf);
+	printf("Read msg (after clear): \"%s\"\n", buf);	
+	close(fd);
+
 	return 0;
 }
